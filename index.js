3e6ebd3b9001ab58ebd3bcd69f529d21ea42f8c0
Dakota Schneider <dakota@hackthetruth.org>
2014-08-31 00:09:23 -0700

    some more changes

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var app = express();
 var cool = require('cool-ascii-faces')
 
 app.use(express.logger());
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/static/libs', express.static(__dirname + '/bower_components'));
+app.use('/css', express.static(__dirname + '/static/css'));
+app.use('/js', express.static(__dirname + '/static/js'));
+app.use('/img', express.static(__dirname + '/static/img'));
+app.use('/js/libs', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(request, response) {
 /*   response.send(cool()); */
@@ -15,16 +17,6 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 
-function go() {
-    setTimeout(function () {
-        app.listen(port, function() {
-            console.log("Listening on " + port);
-            });
-        }, 2000);
-}
-
-try {
-    go();
-} catch (err) {
-    go();
-}
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
