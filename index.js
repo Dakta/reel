674c1d400563328e4c7b07e03910a9ede0ddb0c8
Dakota Schneider <dakota@hackthetruth.org>
2014-09-02 21:47:59 -0700

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 // var http = require('http').Server(app);
 
 
-var port = process.env.port || 5000;
+var port = process.env.port || 3000;
 
 var server = app.listen(port, function(){
     console.log('listening on '+port);
