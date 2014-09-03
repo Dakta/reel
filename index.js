af73aee9511615f78706985c31ddd1d067a21783
Dakota Schneider <dakota@hackthetruth.org>
2014-09-02 21:52:00 -0700

    yeah

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 
 var server = app.listen(app.get('port'), function(){
-    console.log('listening on '+port);
+    console.log('listening on '+app.get('port'));
 });
 
 var io = require('socket.io').listen(server);
