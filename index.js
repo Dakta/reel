946b592a899a6ab8b40e3fc82d7ad584d9da9f8d
Dakota Schneider <dakota@hackthetruth.org>
2014-09-02 21:28:42 -0700

    some fixes

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
+// express config
+app.use('/css', express.static(__dirname + '/static/css'));
+app.use('/js', express.static(__dirname + '/static/js'));
+app.use('/img', express.static(__dirname + '/static/img'));
+app.use('/js/libs', express.static(__dirname + '/bower_components'));
+
+
+/*
 var Engine = require('tingodb')(),
     assert = require('assert');
 
-
 var db = new Engine.Db('./tingodb', {});
 var collection = db.collection("batch_document_insert_collection_safe");
-/*
+
 collection.insert([{hello:'world_safe1'}
   , {hello:'world_safe2'}], {w:1}, function(err, result) {
   assert.equal(null, err);
@@ -23,22 +30,75 @@ collection.insert([{hello:'world_safe1'}
 */
 
 
-app.use('/css', express.static(__dirname + '/static/css'));
-app.use('/js', express.static(__dirname + '/static/js'));
-app.use('/img', express.static(__dirname + '/static/img'));
-app.use('/js/libs', express.static(__dirname + '/bower_components'));
+var mongoose = require('mongoose'),
+    mongoURI = process.env.MONGOLAB_URI || 'oops';
 
-
-app.get('/', function(req, res){
-res.sendfile('index.html');
+// mongoose does this async, which is nice
+mongoose.connect(mongoURI, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + mongoURI + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + mongoURI);
+  }
 });
 
+
+// database schemas
+var userSchema = new mongoose.Schema({
+  name: {
+    first: { type: String, trim: true },
+    last: { type: String, trim: true }
+  },
+  age: { type: Number, min: 0}
+});
+
+var User = mongoose.model('Users', userSchema);
+
+/*
+var newUser = new User({
+    name: {
+        first: 'John',
+        last: ' Doe'
+    },
+    age: 28
+});
+newUser.save(function(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('inserted john doe');
+});
+*/
+
+
+// express routing
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+app.get('/users/:userid/:username?', function(req, res) {
+    console.log(req.params);
+    res.sendfile('index.html');
+});
+
+
+// socket.io websocket stuff
 io.on('connection', function(socket){
     console.log('a user connected');
 
+/*
     collection.findOne({hello:'world_safe2'}, function(err, item) {
         console.log(item);
         socket.emit('raw', item);
+    });
+*/
+
+    User.find({}).exec(function(err, res) {
+        if (!err) {
+            socket.emit('raw', res);
+        } else {
+            console.log('fail: '+err);
+        }
     });
     
     socket.on('setPseudo', function (data) {
